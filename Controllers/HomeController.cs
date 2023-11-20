de95754863c9b6f7cdd5483bac35fa7027181f56
Nigarglm <tu7mk3dbf@code.edu.az>
2023-11-20 04:50:21 +0400

    added new task

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _16Nov_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Slide.cs
@@ -0,0 +1,11 @@
+﻿namespace _16Nov_task.Models
+{
+    public class Slide
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Subtitle { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+}
+}
