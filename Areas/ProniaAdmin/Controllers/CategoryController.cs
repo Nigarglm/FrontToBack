933d33c988fd6047804edb83752e327bd4ce1f23
Nigarglm <tu7mk3dbf@code.edu.az>
2023-12-01 08:05:55 +0400

    update and delete added

--- Areas/ProniaAdmin/Controllers/CategoryController.cs
+++ Areas/ProniaAdmin/Controllers/CategoryController.cs
@@ -44,7 +44,55 @@ namespace _16Nov_task.Areas.ProniaAdmin.Controllers
             await _context.Categories.AddAsync(category);
             await _context.SaveChangesAsync();
 
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
         }
+
+
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Update(int id,  Category category)
+        {
+            if(!ModelState .IsValid) return View();
+
+            Category existed = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (existed == null) return NotFound();
+
+            bool result = _context.Categories.Any(c=>c.Name == category.Name && c.Id!=id);
+
+            if (result)
+            {
+                ModelState.AddModelError("Name", "Bu adli category artiq movcuddur");
+                return View();
+            }
+
+            existed.Name = category.Name;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if(id<=0) return BadRequest();
+
+            Category existed = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (existed == null) return NotFound();
+
+            _context.Categories.Remove(existed);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
