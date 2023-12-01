34589d54a172e1b3f4503f8a148da16f58c71261
Nigarglm <tu7mk3dbf@code.edu.az>
2023-12-01 12:25:41 +0400

    slide table and create table added

---
+++ Areas/ProniaAdmin/Controllers/SlideController.cs
@@ -0,0 +1,70 @@
+﻿using _16Nov_task.DAL;
+using _16Nov_task.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _16Nov_task.Areas.ProniaAdmin.Controllers
+{
+    [Area("ProniaAdmin")]
+    public class SlideController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SlideController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slide> slides = await _context.Slides.ToListAsync();
+            return View(slides);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Slide slide)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            bool result = _context.Categories.Any(c => c.Name.ToLower().Trim() == slide.Title.ToLower().Trim());
+            if (result)
+            {
+                ModelState.AddModelError("Name", "Bele bir category artiq movcuddur");
+                return View();
+            }
+
+            if(slide.Photo == null)
+            {
+                ModelState.AddModelError("Photo", "Mutleq sekil daxil edilmelidir");
+                return View();
+            }
+            if (!slide.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "Fayl tipi uygun deyil");
+                return View();
+            }
+            if (slide.Photo.Length > 2 * 1024 * 1024)
+            {
+                ModelState.AddModelError("Photo", "Faylin hecmi 2 mb-dan cox olmamalidir");
+                return View();
+            }
+
+            FileStream file = new FileStream(@"C:\Users\Work\Desktop\16Nov(FrontToBack)\wwwroot\assets\images\website-images\" + slide.Photo.FileName, FileMode.Create);
+
+            await slide.Photo.CopyToAsync(file);
+            file.Close();
+            slide.Image = slide.Photo.FileName;
+
+
+
+            await _context.Slides.AddAsync(slide);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Models/Slide.cs
+++ Models/Slide.cs
@@ -1,12 +1,21 @@
-﻿namespace _16Nov_task.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _16Nov_task.Models
 {
     public class Slide
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="basliq mutleq daxil edilmelidir")]
+        [MaxLength(50, ErrorMessage ="basliq ucun maksimal uzunluq 50-dir.")]
         public string Title { get; set; }
+        [MaxLength(30)]
         public string Subtitle { get; set; }
+        [MaxLength(250)]
         public string Description { get; set; }
-        public string Image { get; set; }
+        public string? Image { get; set; }
         public int Order { get; set; }
+        [NotMapped]
+        public IFormFile Photo { get; set; }
     }
 }
